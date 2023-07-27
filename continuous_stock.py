45eac9a39ecbd907e5e55668664e4c0745367edf
Jordan Wheeler <jordan.wheeler7@gmail.com>
2023-07-26 21:13:02 -0500

    Updated Stock Output/Input/File and Server File

--- continuous_stock.py
+++ continuous_stock.py
@@ -27,19 +27,18 @@ We usually include a .env-example file to illustrate the format.
 
 # Standard Library
 import asyncio
-from datetime import datetime
-from pathlib import Path
 import os
+from pathlib import Path
+from datetime import datetime
 from random import randint
-import yfinance as yf
 
 # External Packages
 import pandas as pd
+import yfinance as yf
 from collections import deque
-from dotenv import load_dotenv
+
 
 # Local Imports
-from fetch import fetch_from_url
 from util_logger import setup_logger
 
 # Set up a file logger
@@ -47,7 +46,7 @@ logger, log_filename = setup_logger(__file__)
 
 
 def lookup_ticker(company):
-    company_dictionary = {
+    stocks_dictionary = {
         "Cadence Design Systems": "CDNS",
         "Synopys": "SNPS",
         "Tractor Supply Company": "TSCO",
@@ -59,27 +58,39 @@ def lookup_ticker(company):
         "CDW Corporation": "CDW",
         "Intuit Inc.": "INTU",
     }
-    
+    ticker = stocks_dictionary[company]
+    return ticker
 
 async def get_stock_price(ticker):
     logger.info("Calling get_stock_price for {ticker}")
     # stock = yf.Ticker(ticker) # uncomment when ready to use real data #This gets stock data for the ticker
-    # price = stock.history(period="1d").tail(1)["Close"][0] #This gets the closing price for the stock
+    # price = stock.info['currentPrice'] #This gets the closing price for the stock
     price = randint(132, 148)  # This is a random number generator for testing
     return price
 
 # Function to create or overwrite the CSV file with column headings
 def init_csv_file(file_path):
     df_empty = pd.DataFrame(
-        columns=["Company", "Ticker", "Time", "Price"]
+        columns=["Company", "Ticker", "Time", "Stock_Price"]
     )
     df_empty.to_csv(file_path, index=False)    
     
 async def update_csv_stock():
     """Update the CSV file with the latest location information."""
-    logger.info("Calling update_csv_stock_price")
+    logger.info("Calling update_csv_stock")
     try:
-        company = ["Cadence Design Systems", "Synopys", "Tractor Supply Company", "Lululemon Ahtletica Inc.", "Arch Capital Group Ltd.", "Costco Wholesale Corporation", "Brown & Brown Inc.", "Thermo Fisher Scientific Inc.", "CDW Corporation", "Intuit Inc."]
+        companies = [
+            "Cadence Design Systems", 
+            "Synopys", 
+            "Tractor Supply Company", 
+            "Lululemon Ahtletica Inc.", 
+            "Arch Capital Group Ltd.", 
+            "Costco Wholesale Corporation", 
+            "Brown & Brown Inc.", 
+            "Thermo Fisher Scientific Inc.", 
+            "CDW Corporation", 
+            "Intuit Inc.",
+            ]
         update_interval = 60  # Update every 1 minute (60 seconds)
         total_runtime = 15 * 60  # Total runtime maximum of 15 minutes
         num_updates = 10  # Keep the most recent 10 readings
@@ -90,7 +101,7 @@ async def update_csv_stock():
         # Use a deque to store just the last, most recent 10 readings in order
         records_deque = deque(maxlen=num_updates)
 
-        fp = Path(__file__).parent.joinpath("data").joinpath("mtcars_location.csv")
+        fp = Path(__file__).parent.joinpath("data").joinpath("mtcars_stock.csv")
 
         # Check if the file exists, if not, create it with only the column headings
         if not os.path.exists(fp):
@@ -99,7 +110,7 @@ async def update_csv_stock():
         logger.info(f"Initialized csv file at {fp}")
 
         for _ in range(num_updates):  # To get num_updates readings
-            for stock_price in stock_price:
+            for company in companies:
                 ticker = lookup_ticker(company)
                 new_price = await get_stock_price(ticker)
                 time_now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")  # Current time
